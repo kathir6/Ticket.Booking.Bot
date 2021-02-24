ed08c472b00533bc5f54c167d898f5cbb67459dc
stepin104678 <maiananthan98@gmail.com>
2021-02-24 15:15:13 +0530

    functions.cpp deletion

--- Server/functions.cpp
+++
@@ -1,8 +0,0 @@
-#include "server_client.h"
-
-void tomorrow_date() {
-  struct tm time_struct;
-  cout << time_struct.tm_sec << "\n";
-  // time_struct = gmtime();
-  cout << time_struct.tm_sec << "\n";
-}
